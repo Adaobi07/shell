b63eb4b874482cc34473d7ec101f7a406567f0f9
adaobi7 <adaogechi4@gmail.com>
2024-09-02 15:10:12 -0400

    -a

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  set -x
-m
+
 ################
 # Instructions #
 ################
@@ -12,11 +12,11 @@ m
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
 ls
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
-$ touch dir2/file1.txt
-$ touch dir2/file2.txt
-$ touch dir2/file3.txt
-$ touch dir2/file4.txt
-$ touch dir2/file5.txt
+touch dir2/file{1..5}.txt
+touch dir2/file2.txt
+touch dir2/file3.txt
+touch dir2/file4.txt
+touch dir2/file5.txt
 
 
 
