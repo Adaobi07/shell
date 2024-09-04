4946324999603087385849dc3bf6bdee59d303a4
adaobi7 <adaogechi4@gmail.com>
2024-09-04 17:51:38 -0400

    Made correction

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -12,7 +12,7 @@
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
 ls
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
-$ touch dir2/file1.txt file2.txt file3.txt file4.txt file5.txt
+touch dir2/file1.txt file2.txt file3.txt file4.txt file5.txt
 
 
 
